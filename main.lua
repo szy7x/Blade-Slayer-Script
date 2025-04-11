6dcddc45804ae0d54dcd96e4038210e36da2ee16
szy7x <161705680+szy7x@users.noreply.github.com>
2025-04-10 22:57:21 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -26,12 +26,16 @@ local Window = Rayfield:CreateWindow({
    KeySystem = false,
 })
 
+Rayfield:LoadConfiguration()
+
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Workspace = game:GetService("Workspace")
+local player = Players.LocalPlayer
+
 -- ABA: Farm
 local FarmTab = Window:CreateTab("Farm", 4483362458)
 
-Rayfield:LoadConfiguration()
-
--- Toggle Auto Farm
 local AutoFarmToggle = FarmTab:CreateToggle({
    Name = "Auto Farm",
    CurrentValue = false,
@@ -43,29 +47,19 @@ local AutoFarmToggle = FarmTab:CreateToggle({
 
 task.spawn(function()
    while true do
-      task.wait(0.5) -- Espera entre os loops
+      task.wait(0.5)
       if _G.AutoFarm then
          pcall(function()
-            -- Loop para percorrer todos os inimigos
-            for _, mob in ipairs(game:GetService("Workspace").Enemies:GetChildren()) do
-               -- Verifica se o mob possui a parte "HumanoidRootPart"
+            for _, mob in ipairs(Workspace.Enemies:GetChildren()) do
                if mob:FindFirstChild("HumanoidRootPart") then
-                  -- Teleporte para o inimigo
-                  local humanoidRootPart = mob:FindFirstChild("HumanoidRootPart")
-                  if humanoidRootPart then
-                     -- Atualiza a posição do jogador para o CFrame do inimigo
-                     local playerHRP = game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
-                     if playerHRP then
-                        -- Teleporta o jogador para a posição do inimigo
-                        playerHRP.CFrame = humanoidRootPart.CFrame * CFrame.new(0, 3, 0) -- Ajuste de altura (3 studs acima do inimigo)
-                        wait(0.5)  -- Aguarda para garantir o teleporte
-
-                        -- Verifica se o inimigo ainda está vivo
-                        local humanoid = mob:FindFirstChild("Humanoid")
-                        if humanoid and humanoid.Health > 0 then
-                           -- Aplica o dano ao inimigo
-                           game:GetService("ReplicatedStorage").Remotes.DamageMonster:FireServer(mob)
-                        end
+                  local humanoidRootPart = mob.HumanoidRootPart
+                  local playerHRP = player.Character and player.Character:FindFirstChild("HumanoidRootPart")
+                  if playerHRP then
+                     playerHRP.CFrame = humanoidRootPart.CFrame * CFrame.new(0, 3, 0)
+                     wait(0.5)
+                     local humanoid = mob:FindFirstChild("Humanoid")
+                     if humanoid and humanoid.Health > 0 then
+                        ReplicatedStorage.Remotes.DamageMonster:FireServer(mob)
                      end
                   end
                end
@@ -78,7 +72,6 @@ end)
 -- ABA: Player
 local PlayerTab = Window:CreateTab("Player", 4483362458)
 
--- Toggle Auto Rebirth
 PlayerTab:CreateToggle({
    Name = "Auto Rebirth",
    CurrentValue = false,
@@ -93,13 +86,12 @@ task.spawn(function()
       task.wait(2)
       if _G.AutoRebirth then
          pcall(function()
-            game:GetService("ReplicatedStorage").Remotes.PlayerReborn:FireServer()
+            ReplicatedStorage.Remotes.PlayerReborn:FireServer()
          end)
       end
    end
 end)
 
--- Toggle Auto Equip Best Weapon
 PlayerTab:CreateToggle({
    Name = "Auto Equip Best Weapon",
    CurrentValue = false,
@@ -114,7 +106,7 @@ task.spawn(function()
       task.wait(2)
       if _G.AutoEquip then
          pcall(function()
-            game:GetService("ReplicatedStorage").Remotes.EquipBestWeapon:FireServer()
+            ReplicatedStorage.Remotes.EquipBestWeapon:FireServer()
          end)
       end
    end
@@ -123,7 +115,6 @@ end)
 -- ABA: Heroes
 local HeroesTab = Window:CreateTab("Heroes", 4483362458)
 
--- Toggle Auto Equip Best Hero
 HeroesTab:CreateToggle({
    Name = "Auto Equip Best Hero",
    CurrentValue = false,
@@ -138,13 +129,12 @@ task.spawn(function()
       task.wait(2)
       if _G.AutoEquipBestHero then
          pcall(function()
-            game:GetService("ReplicatedStorage").Remotes.AutoEquipBestHero:FireServer()
+            ReplicatedStorage.Remotes.AutoEquipBestHero:FireServer()
          end)
       end
    end
 end)
 
--- Toggle Auto Hatch Nearest
 HeroesTab:CreateToggle({
    Name = "Auto Hatch Nearest",
    CurrentValue = false,
@@ -159,17 +149,16 @@ task.spawn(function()
       task.wait()
       if _G.AutoHatchNearest then
          pcall(function()
-            local mapsFolder = game.Workspace:FindFirstChild("Maps")
+            local mapsFolder = Workspace:FindFirstChild("Maps")
             if mapsFolder then
                local currentMap = mapsFolder:GetChildren()[1]
                if currentMap and currentMap:FindFirstChild("Map") and currentMap.Map:FindFirstChild("Eggs") then
                   local egg = currentMap.Map.Eggs:GetChildren()[1]
-                  game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = egg.CFrame
+                  player.Character.HumanoidRootPart.CFrame = egg.CFrame
                   wait(0.5)
-                  local vim = game:GetService("VirtualInputManager")
-                  vim:SendKeyEvent(true, "E", false, game)
+                  game:GetService("VirtualInputManager"):SendKeyEvent(true, "E", false, game)
                   wait(0.2)
-                  vim:SendKeyEvent(false, "E", false, game)
+                  game:GetService("VirtualInputManager"):SendKeyEvent(false, "E", false, game)
                end
             end
          end)
@@ -177,95 +166,102 @@ task.spawn(function()
    end
 end)
 
--- ABA: Trade
-local TradeTab = Window:CreateTab("Trade", 4483362458)
+-- ABA: Dungeon
+local DungeonTab = Window:CreateTab("Dungeon", 4483362458)
 
-local players = game:GetService("Players")
-local player = game.Players.LocalPlayer
-local tradeInProgress = false
-local tradeOffer = {
-   target = nil,
-   heroesOffered = {}
+local raidVars = {
+   s = "Room1",
+   diff = 1,
+   mapid = 50201,
+   table = {"Room1", "Room2", "Room3"},
+   dtable = {"1", "2", "3"},
+   fraid = false,
+   hero = {guid = "null", index = 1, skill = true, id = 101},
 }
 
--- Função para enviar oferta de troca
-local function sendTradeOffer(targetPlayerName)
-    local targetPlayer = players:FindFirstChild(targetPlayerName)
-    if targetPlayer then
-        if not tradeInProgress then
-            tradeInProgress = true
-            tradeOffer.target = targetPlayer
-            print("Troca enviada para " .. targetPlayer.Name)
-            -- Aqui você pode definir os heróis que está oferecendo
-            tradeOffer.heroesOffered = {"Hero1", "Hero2"} -- exemplo de heróis
-        else
-            print("Já existe uma troca em andamento!")
-        end
-    else
-        print("Jogador não encontrado!")
-    end
-end
-
--- Função para aceitar troca
-local function acceptTrade()
-    if tradeInProgress and tradeOffer.target then
-        -- Adiciona os heróis ao inventário do jogador
-        print("Troca aceita com " .. tradeOffer.target.Name)
-        for _, hero in ipairs(tradeOffer.heroesOffered) do
-            -- Aqui você pode adicionar o herói ao inventário
-            print("Adicionando " .. hero .. " ao seu inventário.")
-            -- Código para adicionar o herói ao inventário
-        end
-
-        -- Enviar os heróis ao inventário do outro jogador, sem removê-los do próprio
-        for _, hero in ipairs(tradeOffer.heroesOffered) do
-            print("Enviando " .. hero .. " para " .. tradeOffer.target.Name)
-            -- Código para adicionar o herói ao inventário do outro jogador
-        end
-        
-        tradeInProgress = false
-    else
-        print("Nenhuma troca pendente para aceitar.")
-    end
-end
-
--- Função para atualizar o dropdown com os jogadores no servidor
-local function updatePlayerList()
-    local playerList = {}
-    for _, p in pairs(players:GetPlayers()) do
-        if p.Name ~= player.Name then
-            table.insert(playerList, p.Name)
-        end
-    end
-    return playerList
-end
-
--- Caixa de seleção para escolher o jogador para a troca
-local PlayerDropdown = TradeTab:CreateDropdown({
-   Name = "Escolher Jogador para Troca",
-   Options = updatePlayerList(),
-   CurrentOption = updatePlayerList()[1],
-   Flag = "TradePlayerDropdown",
-   Callback = function(selectedPlayer)
-      sendTradeOffer(selectedPlayer)
-   end,
+DungeonTab:CreateDropdown({
+   Name = "Select Room",
+   Options = raidVars.table,
+   CurrentOption = raidVars.s,
+   Callback = function(v) raidVars.s = v end,
 })
 
--- Verificar se o botão de enviar troca está visível
-TradeTab:CreateButton({
-   Name = "Envie a Troca Manualmente Pelo Jogo",
-   Callback = function()
-      local selectedPlayer = PlayerDropdown:GetSelected() -- Obtém o jogador selecionado no dropdown
-      sendTradeOffer(selectedPlayer)
-   end,
+DungeonTab:CreateDropdown({
+   Name = "Select difficulty",
+   Options = raidVars.dtable,
+   CurrentOption = tostring(raidVars.diff),
+   Callback = function(v) raidVars.diff = tonumber(v) end,
 })
 
--- Botão para aceitar troca
-TradeTab:CreateButton({
-   Name = "Dupe Heroes - 1 CLICK ONLY",
+DungeonTab:CreateInput({
+   Name = "Insert map ID",
+   PlaceholderText = "Ex: 50201",
+   RemoveTextAfterFocusLost = false,
+   Callback = function(v) raidVars.mapid = tonumber(v) end,
+})
+
+DungeonTab:CreateButton({
+   Name = "Raid lobby",
    Callback = function()
-      acceptTrade()
-   end,
+      ReplicatedStorage.Remotes.LocalPlayerTeleport:FireServer({mapId = 50201})
+   end
+})
+
+DungeonTab:CreateButton({
+   Name = "Start raid",
+   Callback = function()
+      Rayfield:Notify({Title = "Raid", Content = "Don't move, auto kill is enabled.", Duration = 5})
+      ReplicatedStorage.Remotes.EnterRaidRoom:FireServer(raidVars.s)
+      wait(0.1)
+      ReplicatedStorage.Remotes.SelectRaidsDifficulty:FireServer({difficulty = raidVars.diff, roomName = raidVars.s, selectMapId = raidVars.mapid})
+      wait(0.1)
+      ReplicatedStorage.Remotes.StartChallengeRaidMap:InvokeServer({userIds = {player.UserId}, roomName = raidVars.s})
+   end
+})
+
+DungeonTab:CreateToggle({
+   Name = "Start raid + Auto kill",
+   CurrentValue = false,
+   Callback = function(v)
+      raidVars.fraid = v
+      if v then
+         Rayfield:Notify({Title = "Raid", Content = "Auto Kill ON - Não se mova!", Duration = 5})
+         ReplicatedStorage.Remotes.EnterRaidRoom:FireServer(raidVars.s)
+         wait(0.1)
+         ReplicatedStorage.Remotes.SelectRaidsDifficulty:FireServer({difficulty = raidVars.diff, roomName = raidVars.s, selectMapId = raidVars.mapid})
+         wait(0.1)
+         ReplicatedStorage.Remotes.StartChallengeRaidMap:InvokeServer({userIds = {player.UserId}, roomName = raidVars.s})
+         wait(0.5)
+         task.spawn(function()
+            while raidVars.fraid do
+               task.wait()
+               if #Workspace.Enemys:GetChildren() < 1 then
+                  if Workspace:FindFirstChild("EnchantChest") then
+                     firetouchinterest(Workspace.EnchantChest.Part, player.Character.HumanoidRootPart, 0)
+                     wait(0.1)
+                     firetouchinterest(Workspace.EnchantChest.Part, player.Character.HumanoidRootPart, 1)
+                     ReplicatedStorage.Remotes.QuitRaidsMap:InvokeServer({currentSlotIndex = 1, toMapId = 50201})
+                     raidVars.fraid = false
+                  else
+                     Rayfield:Notify({Title = "Raid", Content = "Pls join raid. #Failed", Duration = 5})
+                     raidVars.fraid = false
+                  end
+               else
+                  for _, get in ipairs(Workspace.Enemys:GetChildren()) do
+                     local guid = get:GetAttribute("EnemyGuid")
+                     ReplicatedStorage.Remotes.PlayerClickAttack:FireServer(guid)
+                     ReplicatedStorage.Remotes.PlayerRespirationSkillAttack:InvokeServer(guid)
+                     if raidVars.hero.guid ~= "null" then
+                        ReplicatedStorage.Remotes.ClickEnemy:InvokeServer(guid)
+                        ReplicatedStorage.Remotes.HeroSkillHarm:FireServer({harmIndex = raidVars.hero.index, isSkill = raidVars.hero.skill, heroGuid = raidVars.hero.guid, skillId = raidVars.hero.id})
+                        ReplicatedStorage.Remotes.RespirationSkillHarm:FireServer({harmIndex = raidVars.hero.index, skillId = raidVars.hero.id})
+                     end
+                  end
+               end
+            end
+         end)
+      end
+   end
 })
 
 -- Finalização
